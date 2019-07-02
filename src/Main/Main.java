2f5e465b6fa976f432aae6860b8ab5581497d447
radames <radames@x50>
2019-07-02 17:33:54 -0300

    IMC

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 /**
@@ -15,18 +10,20 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-        
+
         //entrada de dados
-        int achico = 150;
-        int aBento = 110;
-        
-        
+        Entrada entrada = new Entrada();
+        double peso = entrada.lerNumeroDouble("Digite o peso");
+        double altura = entrada.lerNumeroDouble("Digite a altura");
+
         Processamento processamento = new Processamento();
-       int resposta = processamento.quantosAnos(achico,aBento);
+        double imc = processamento.calcularIMC(peso, altura);
+
+        String classificacao = processamento.classificarIMC(imc);
         
-       Saida saida = new Saida();
-        saida.imprimirNumeroInteiro(resposta, "Quantidade de anos");
+        Saida saida = new Saida();
+        saida.imprimirNumeroDouble(imc, "IMC");
+        saida.imprimirAvisoString(classificacao);
     }
 
 }
--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -17,4 +17,32 @@ class Processamento {
         return cont;
     }
 
+    public double calcularIMC(double peso, double altura) {
+        double imc = 0;
+        imc = peso / (altura * altura);
+        return imc;
+    }
+
+    public String classificarIMC(double imc) {
+        String classificacao = "";
+        if (imc < 16) {
+            classificacao = "abaixo do peso grau III";
+        } else if (imc < 17) {
+            classificacao = "abaixo do peso grau II";
+        } else if (imc < 18.5) {
+            classificacao = "abaixo do peso grau I";
+        } else if (imc < 25) {
+            classificacao = "peso ideal";
+        } else if (imc < 30) {
+            classificacao = "sobre peso";
+        } else if (imc < 35) {
+            classificacao = "obesidade grau I";
+        } else if (imc < 40) {
+            classificacao = "obesidade grau II";
+        } else {
+            classificacao = "obesidade grau III";
+        }
+        return classificacao;
+    }
+
 }
