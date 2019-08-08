2fc0233c6fc5c2459e3db131aa7e2e453b0f8cbe
Radames J Halmeman <rjhalmeman@gmail.com>
2019-08-08 14:02:06 -0300

    Lp1_2bim_Q1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,21 +1,34 @@
 package Main;
 
-/**
- * @author Radames J Halmeman - rjhalmeman@gmail.com
+/*
+ Escreva um programa que leia, para um número indeterminado de alunos, o código do aluno e três notas. 
+Calcule a média do aluno e mostre o código do aluno, suas três notas, a média do aluno e uma mensagem de 
+"APROVADO" se a média for maior ou igual a 5 e "REPROVADO" se a média for menor que 5. 
+Repita a operação até que o código de aluno lido seja negativo. Imprima a média da turma. (6 pontos)
  */
 public class Main {
-
+    
     public static void main(String[] args) {
         Entrada entrada = new Entrada();
-        double a = entrada.lerNumeroDouble("Digite um valor para A");
-        double b = entrada.lerNumeroDouble("Digite um valor para B");
-
         Processamento processamento = new Processamento();
-        Double soma = processamento.getSoma(a, b);
-
         Saida saida = new Saida();
-        saida.imprimirNumeroDouble("Resultado da soma", soma);
-
+        
+        while (true) {
+            double cod = entrada.lerNumeroDouble("Digite o código");
+            if (cod < 0) {
+                break;
+            } else {
+                double n1 = entrada.lerNumeroDouble("N1");
+                double n2 = entrada.lerNumeroDouble("N2");
+                double n3 = entrada.lerNumeroDouble("N3");
+                double mediaAluno = processamento.mediaAluno(n1, n2, n3);
+                String situacao = processamento.getSituacao(mediaAluno);
+                saida.imprimirAvisoString(cod + " - " + n1 + " - " + n2 + " - " + n3 + " - " + mediaAluno + " - " + situacao);
+            }
+        }
+        
+        saida.imprimirNumeroDouble("Média da turma", processamento.getMediaTurma());
+        
     }
-
+    
 }
--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -3,8 +3,25 @@ package Main;
 //@author Radames J Halmeman  - rjhalmeman@gmail.com
 class Processamento {
 
-    public Double getSoma(double a, double b) {
-        double resultado = a + b;
-        return resultado;
+    private double somaMedias = 0;
+    private int cont = 0;
+
+    public double mediaAluno(double n1, double n2, double n3) {
+        double m = (n1 + n2 + n3) / 3;
+        somaMedias += m;
+        cont++;
+        return m;
+    }
+
+    public String getSituacao(double mediaAluno) {
+        if (mediaAluno < 5) {
+            return "Reprovado";
+        } else {
+            return "Aprovado";
+        }
+    }
+
+    public double getMediaTurma() {
+        return somaMedias / cont;
     }
 }
