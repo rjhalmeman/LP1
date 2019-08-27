e7d91ca15140bf7a316b552b217328ffb9748a55
radames <radames@x50>
2019-08-27 15:52:08 -0300

    ex 7 lista 31

--- src/Main/Processamento.java
+++ src/Main/Processamento.java
@@ -3,48 +3,47 @@ package Main;
 //@author Radames J Halmeman  - rjhalmeman@gmail.com
 class Processamento {
 
-    
-
     String nomesDosDigitos(int n) {
         String sn = String.valueOf(n);
+        System.out.println("sn " + sn);
         String aux = "";
-        String resp ="";
-        for (int i = 0; i < sn.length(); i++) {
-            switch (Integer.valueOf(sn.charAt(i))) {
-                case 0:
-                    aux=" zero";
+        String resp = "";
+        for (int i = 0; i < sn.length(); i++) {               
+            switch (Character.valueOf(sn.charAt(i))) {
+                case '0':
+                    aux = " zero";
                     break;
-                case 1:
-                    aux=" um";
+                case '1':
+                    aux = " um";
                     break;
-                case 2:
-                    aux=" dois";
+                case '2':
+                    aux = " dois";
                     break;
-                case 3:
-                    aux=" três";
+                case '3':
+                    aux = " três";
                     break;
-                case 4:
-                    aux=" quatro";
+                case '4':
+                    aux = " quatro";
                     break;
-                case 5:
-                    aux=" cinco";
+                case '5':
+                    aux = " cinco";
                     break;
-                case 6:
-                    aux=" seis";
+                case '6':
+                    aux = " seis";
                     break;
-                case 7:
-                    aux=" sete";
+                case '7':
+                    aux = " sete";
                     break;
-                case 8:
-                    aux=" oito";
+                case '8':
+                    aux = " oito";
                     break;
-                case 9:
-                    aux=" nove";
+                case '9':
+                    aux = " nove";
                     break;
                 default:
-                   aux = " erro ";
+                    aux = " erro ";
             }
-            resp+=aux;
+            resp += aux;
         }
         return resp;
     }
